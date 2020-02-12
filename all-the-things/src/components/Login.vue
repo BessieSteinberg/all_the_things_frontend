8e1ee821df76a6cfc0570cfa33bf96218cbfaffc
Bessie Steinberg <5060285+BessieSteinberg@users.noreply.github.com>
2020-02-12 12:55:34 -0700

    initial pass at login form

---
+++ all-the-things/src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="login">
+        <h1>Log In</h1>
+        <div class="field">
+            <label class="label">Username</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Text input">
+            </div>
+        </div>
+        <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+                <input class="input" type="text">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style lang="scss" scoped>
+    .login {
+        text-align: center;
+    }
+</style>
--- all-the-things/src/views/Home.vue
+++ all-the-things/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is the home component</h1>
+    <Login />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Login from '@/components/Login.vue';
 
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
-    HelloWorld,
+    Login,
   },
 };
 </script>
